2543ce7739c7eab2a607ad1ec99a9a68aa59a31f
tomal-g <tomal.isidore@brainstation-23.com>
2026-07-10 19:20:21 +0600

    temp: TC0146, TC0147, TC0157 with dynamic registration

--- tests/accountServices/contactInfoPage.spec.js
+++ tests/accountServices/contactInfoPage.spec.js
@@ -1,21 +1,48 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../../pages/login/loginpage";
+import { RegistrationPage } from "../../pages/registration/registrationpage";
+import { OpenAccountsPage } from "../../pages/accountServices/openAccountsPage";
 import { ContactInfoPage } from "../../pages/accountServices/contactInfoPage";
 
 test.describe("Update Contact Info", () => {
-  let loginPage, openAccountsPage, contactInfoPage;
+  let username, password, contactInfoPage;
+
+  test.beforeAll(async ({ browser }) => {
+    const page = await browser.newPage();
+    const registrationPage = new RegistrationPage(page);
+
+    await registrationPage.goto();
+
+    const uniqueId = Date.now();
+    username = `user${uniqueId}`;
+    password = `Pass${uniqueId}`;
+
+    await registrationPage.registration(
+      "John",
+      "Doe",
+      "123 Main St",
+      "Dhaka",
+      "Dhaka",
+      "1200",
+      "1234",
+      "123456789",
+      username,
+      password,
+      password,
+    );
+    await page.close();
+  });
 
   test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
+    const loginPage = new LoginPage(page);
     contactInfoPage = new ContactInfoPage(page);
     await loginPage.goto();
-    await loginPage.login("Bushra", "123");
+    await loginPage.login(username, password);
   });
   test("TC0157 - Update all contact fields with valid data", async ({
     page,
   }) => {
     await contactInfoPage.clickUpdateProfileLink();
-
     await contactInfoPage.updateContactInfo({
       firstName: "nobody",
       lastName: "1234",
