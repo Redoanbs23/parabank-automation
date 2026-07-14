e84ef972fc48a5821c7fb6c3578ea847fede47e7
Bushra <rabiya.bushra@brainstation-23.com>
2026-07-14 15:08:46 +0600

    test: add GET account details API test case

--- tests/API/customeraccountAPI.spec.js
+++ tests/API/customeraccountAPI.spec.js
@@ -22,6 +22,8 @@ test.describe('Account Service API', () => {
     await expect(page.getByText('Your account was created successfully')).toBeVisible();
   });
 
+  //Verify that GET customer accounts returns 200 OK with a valid array of accounts for a valid customerId
+
   test('Verify that GET customer accounts returns 200 OK with a valid array of accounts for a valid customerId', async ({ request }) => {
     const loginResponse = await request.get(`${base_url}/login/${username}/${password}`, {
       headers: { 'Accept': 'application/json' }
@@ -48,4 +50,45 @@ test.describe('Account Service API', () => {
       expect(account).toHaveProperty('balance');
     }
   });
+
+test('Verify that GET account details returns 200 OK with the correct account object for a valid accountId', async ({ request }) => {
+
+   
+    const loginResponse = await request.get(`${base_url}/login/${username}/${password}`, {
+      headers: { 'Accept': 'application/json' }
+    });
+    expect(loginResponse.status()).toBe(200);
+
+    const customer = await loginResponse.json();
+    const customerId = customer.id;
+    console.log('Customer ID:', customerId);
+
+    
+    const accountsResponse = await request.get(`${base_url}/customers/${customerId}/accounts`, {
+      headers: { 'Accept': 'application/json' }
+    });
+    expect(accountsResponse.status()).toBe(200);
+
+    const accounts = await accountsResponse.json();
+    expect(accounts.length).toBeGreaterThan(0);
+
+    const accountId = accounts[0].id;
+    console.log('Valid Account ID:', accountId);
+
+    const response = await request.get(`${base_url}/accounts/${accountId}`, {
+      headers: { 'Accept': 'application/json' }
+    });
+    expect(response.status()).toBe(200);
+
+    const body = await response.json();
+    console.log('Account details:', body);
+
+   
+    expect(body).toHaveProperty('id', accountId);
+    expect(body).toHaveProperty('customerId', customerId);
+    expect(body).toHaveProperty('type');
+    expect(body).toHaveProperty('balance');
+    expect(typeof body.balance).toBe('number');
+  });
+
 });
